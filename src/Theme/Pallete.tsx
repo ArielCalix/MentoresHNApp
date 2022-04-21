6ead058acd58853418bf1b13ba5e79e21231690e
Ariel Calix <acalixvasquez@gmail.com>
2022-04-21 16:42:11 -0600

    Add - Package y Estilos Instalacion de styled-components y definicion de paleta de colores.

---
+++ src/Theme/Pallete.tsx
@@ -0,0 +1,29 @@
+export const Theme = {
+    /* Pallet Main Colors */
+    lightShades: "#f7f6f7",
+    darkAccent: "#735134",
+    mainBrand: "#364959",
+    lightAccent: "#d9b9a7",
+    darkShades: "#593622",
+    lightShadesTransparent: "#f7f6f780",
+    darkAccentTransparent: "#73513480",
+    mainBrandTransparent: "#36495980",
+    lightAccentTransparent: "#d9b9a780",
+    darkShadesTransparent: "#59362280",
+    /* Color Semantics */
+    primary: "#364959",
+    info: "#593622",
+    success: "#459053",
+    warning: "#c3801b",
+    danger: "#f44336",
+    primaryTransparent: "#36495980",
+    infoTransparent: "#59362280",
+    successTransparent: "#45905340",
+    warningTransparent: "#c3801b40",
+    dangerTransparent: "#f4433640",
+    /* Colores de Texto */
+    textColor: "#f7f6f7",
+    textColorTransparent: "#f7f6f780",
+    textColorDark: "#364959",
+    textColorDarkTransparent: "#36495980",
+}
